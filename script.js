5651b05e8e1201d62988e390ac72ff18f8ec9c04
Steven <steven@example.com>
2026-05-20 19:53:49 -0400

    Add custom grid functionality
    Moved code to create a grid inside generateGrid
    function in order to improve readibility and
    resuability, as it is now required in two
    different places in the program
    
    User is now able to create their own grid by
    selecting the number of cells per side.

--- script.js
+++ script.js
@@ -1,21 +1,30 @@
-// Create 16 x 16 grid
-const container = document.getElementById("container");
+function generateGrid(numCells) {
+  // Set initial grid size
+  container.style.width = `${numCells * 70 + 50}px`;
+  container.style.height = `${numCells * 70}px`;
 
-for (let column = 0; column < 16; column++) {
-  for (let row = 0; row < 16; row++) {
-    const cell = document.createElement("div");
-    container.appendChild(cell);
+  // Generate Grid
+  for (let column = 0; column < numCells; column++) {
+    for (let row = 0; row < numCells; row++) {
+      const cell = document.createElement("div");
+      cell.textContent = `${row + 1}`;
+      container.appendChild(cell);
+    }
   }
+
+  const cells = container.querySelectorAll("div");
+
+  // Add pixelated trail effect
+  cells.forEach((c) => {
+    c.addEventListener("mouseenter", () => {
+      c.style.backgroundColor = "yellow";
+    });
+  });
 }
 
-// Hover effect
-const cells = container.querySelectorAll("div");
-
-cells.forEach((cell) => {
-  cell.addEventListener("mouseenter", () => {
-    cell.style.backgroundColor = "yellow";
-  });
-});
+// Create default 16 x 16 grid
+const container = document.getElementById("container");
+generateGrid(16);
 
 // User Prompt
 const body = document.querySelector("body");
@@ -28,5 +37,8 @@ btnPrompt.addEventListener("click", () => {
   const numCells = +prompt(
     "Type the number of squares you would like per side:",
   );
-  cells.forEach((cell) => cell.remove());
+  const cells = container.querySelectorAll("div");
+
+  cells.forEach((cell) => cell.remove()); // Fix
+  generateGrid(numCells);
 });
